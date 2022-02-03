819c859f83251bb497b32b32e7f8da64811b5240
dr-JT <jtsukahara3@gatech.edu>
2022-02-03 11:55:06 -0500

    classic styling

--- R/table_descriptives.R
+++ R/table_descriptives.R
@@ -27,7 +27,6 @@ table_descriptives <- function(x) {
   table <- knitr::kable(table, digits=2, format="html",
                         caption="Descriptive Statistics")
   tabale <- kableExtra::kable_classic(table, position = "left")
-  table <- kableExtra::kable_styling(table)
   table <- kableExtra::footnote(table, general_title = "\n",
                                 general = paste("Total N = ", N, sep = ""))
   return(table)
