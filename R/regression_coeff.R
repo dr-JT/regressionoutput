5a18f78ee7f71140f81b47ec49b6d897cb8cf0a1
dr-JT <jtsukahara3@gatech.edu>
2022-02-03 08:51:07 -0500

    fix footnote order

--- R/regression_coeff.R
+++ R/regression_coeff.R
@@ -105,14 +105,17 @@ regression_coeff <- function(x, y = NULL, z = NULL,
   }
 
   if (is.null(y) & is.null(z)) {
-    table <- kableExtra::footnote(number = paste("\\\\tiny{*", "H1: ", deparse(x_formula), sep = ""),
+    table <- kableExtra::footnote(table,
+                                  number = paste("\\\\tiny{*", "H1: ", deparse(x_formula), sep = ""),
                                   escape = FALSE)
   } else if (!is.null(y) & is.null(z)) {
-    table <- kableExtra::footnote(number = c(paste("\\\\tiny{*", "H1: ", deparse(x_formula), sep = ""),
+    table <- kableExtra::footnote(table,
+                                  number = c(paste("\\\\tiny{*", "H1: ", deparse(x_formula), sep = ""),
                                              paste("\\\\tiny{*", "H2: ", deparse(y_formula), sep = "")),
                                   escape = FALSE)
   } else {
-    table <- kableExtra::footnote(number = c(paste("\\\\tiny{*", "H1: ", deparse(x_formula), sep = ""),
+    table <- kableExtra::footnote(table,
+                                  number = c(paste("\\\\tiny{*", "H1: ", deparse(x_formula), sep = ""),
                                              paste("\\\\tiny{*", "H2: ", deparse(y_formula), sep = ""),
                                              paste("\\\\tiny{*", "H3: ", deparse(z_formula), sep = "")),
                                   escape = FALSE)
