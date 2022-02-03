575182500fe10f3a21bff10d87bcd932f0f5b34c
dr-JT <jtsukahara3@gatech.edu>
2022-02-03 14:24:11 -0500

    support categorical variables

--- R/get_standardized.R
+++ R/get_standardized.R
@@ -9,7 +9,7 @@
 
 get_standardized <- function(x) {
   x_data <- insight::get_data(x)
-  x_data <- dplyr::mutate_all(x_data, scale)
+  x_data <- dplyr::mutate_if(x_data, is.numeric, scale)
   x_formula <- insight::find_formula(x)$conditional
   model_standardized <- lm(x_formula, data = x_data)
   return(model_standardized)
